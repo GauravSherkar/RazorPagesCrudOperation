92c4bd3cc63e7df5ddc5c724126eb7b5f0206f00
gauravsherkar851@gmail.com <gauravsherkar851@gmail.com>
2023-12-21 11:07:28 +0530

    first commit

---
+++ BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,33 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db=db;
+        }
+        public void OnGet()
+        {
+            
+        }
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(Category);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created Successfully";
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
